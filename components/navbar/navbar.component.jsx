ea00d01935410edd6998668dcd84b100105a4151
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-08-12 17:48:48 +0900

    Create Navbar

---
+++ components/navbar/navbar.component.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav>
+        <div>
+          <Link href="/">
+            <a title="Our API"> Home </a>
+          </Link>
+          <Link href="/about">
+            <a title="About Next JS"> About NextJS </a>
+          </Link>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
