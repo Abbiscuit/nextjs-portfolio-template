654e00a96154c2017ff565cf843f227b506e9a94
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-08-12 17:42:46 +0900

    Initial Commit & Setup basic routing

---
+++ pages/about.js
@@ -0,0 +1,27 @@
+const About = () => (
+  <section>
+    <h1>About NextJS</h1>
+    <p>
+      <span>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa earum
+        similique pariatur. Ducimus, earum tenetur. Consectetur, hic placeat
+        rerum facilis doloremque aut alias ab provident neque voluptatem, et
+        deleniti molestias.
+      </span>
+      <span>
+        Ab ea beatae suscipit in accusantium non dolore. Eaque voluptatibus a
+        reprehenderit provident vitae neque, veniam nam tempora omnis ratione
+        quidem dolores reiciendis molestias dolore minus culpa explicabo
+        molestiae et.
+      </span>
+      <span>
+        Repellendus temporibus omnis repudiandae, hic id, dicta illum et ratione
+        pariatur fugiat rem laboriosam, reprehenderit aspernatur asperiores
+        possimus labore quas! Eum pariatur sunt nulla dignissimos debitis
+        reiciendis minima tenetur laudantium.
+      </span>
+    </p>
+  </section>
+);
+
+export default About;
