abeed2051f97245ac6a462977c125933d9aba8ab
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-08-12 17:52:39 +0900

    Add style-jsx just for a try

--- components/navbar/navbar.component.jsx
+++ components/navbar/navbar.component.jsx
@@ -13,6 +13,15 @@ class Navbar extends Component {
             <a title="About Next JS"> About NextJS </a>
           </Link>
         </div>
+        <style jsx>
+          {`
+            a {
+              padding: 10px;
+              text-decoration: none;
+              color: #333;
+            }
+          `}
+        </style>
       </nav>
     );
   }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 
+import Navbar from '../components/navbar/navbar.component';
+
 const Index = () => (
   <section>
+    <Navbar />
     <h1>Hello World from Next JS</h1>
-    <Link href="/about">
-      <a title="About NextJS"> About NextJS Project </a>
-    </Link>
   </section>
 );
 
