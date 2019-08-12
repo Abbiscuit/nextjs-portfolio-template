9213c99fa5ee2ae6edace368993a9459ef2048aa
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-08-12 23:03:11 +0900

    Fix meta description #3

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en, ja">
+      <Html lang="en">
         <Head />
         <body>
           <Main />
