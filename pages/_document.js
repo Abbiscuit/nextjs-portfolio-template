2714ae63b22ad6e90ae75aa7f1d075728244974a
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-08-12 23:09:20 +0900

    Add meta description

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta
+            name="description"
+            content="Simple NextJS Portfolio Template Page"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="author" content="abbiscuitcom" />
+        </Head>
         <body>
           <Main />
           <NextScript />
